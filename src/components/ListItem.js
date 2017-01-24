a181996a8677dae66d6313de76c1d51dd3de58d8
Seungjae Moon <sjaymoon15@gmail.com>
2017-01-24 17:44:45 -0500

    create action creator

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
@@ -20,6 +22,6 @@ const styles = {
     fontSize: 18,
     paddingLeft: 15
   }
-}
+};
 
-export default ListItem;
+export default connect(null, actions)(ListItem);
